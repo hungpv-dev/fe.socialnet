9ad2b1dcfddb7e292dbf827af0b0fa73b26adc03
trangtttph3661 <trangtttph36617@fpt.edu.vn>
2024-08-25 21:38:56 +0700

    login setting

---
+++ src/pages/Client/SettingRePass/index.jsx
@@ -0,0 +1,9 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import './main.scss';
+
+import classNames from "classnames/bind";
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
